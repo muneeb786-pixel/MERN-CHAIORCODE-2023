6ce247a2c61e5f4d80f4468b10cd3201171f5a7f
muneeb786-pixel <muneeburr451@gmail.com>
2024-01-07 21:17:51 +0500

    Theme toggle

---
+++ 09themeToggle/src/context/Theme.js
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { createContext } from "react";
+
+
+export const ThemeContext = createContext({
+    themeMode:'light',
+    lightTheme : ()=>{},
+    darkTheme : () => {}
+})
+
+export const ThemeProvider = ThemeContext.Provider;
+
+export const useTheme = () => {
+    return useContext(ThemeContext)
+}
